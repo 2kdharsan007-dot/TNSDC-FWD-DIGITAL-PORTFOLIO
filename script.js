4ea40cce2208b40e1c997b9c7bbd1d24d9989132
2kdharsan007-dot <2k.dharsan.007@gmail.com>
2025-08-31 20:35:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript placeholder
+// Ippo script empty irukku.
+// Future la form validation, smooth scroll, animations add panna use pannalam.
+
+console.log("APR Fitness Website Loaded ✅");
